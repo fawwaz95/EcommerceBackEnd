077b2fa18f2af7077bcca8d82862ce5502e060e6
fawwaz95 <fawwaz_95@hotmail.com>
2023-09-26 23:17:32 -0400

    added import path from path

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ import "../db/loadEnvironment.mjs";
 import records from "../routes/record.mjs";
 import conn from "../db/conn.mjs";
 import { Db } from "mongodb";
+import path from "path";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require("path");
 
 app.use(cors());
 app.use(express.json());
