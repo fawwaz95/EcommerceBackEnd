8f4759ee91edf7674b7f563167c72e3003dd9cff
fawwaz95 <fawwaz_95@hotmail.com>
2023-10-17 23:21:57 -0400

    Created checkout endpoint, installed stripe, partially implented stripe checkout session within endpoint

--- routes/record.mjs
+++ routes/record.mjs
@@ -2,9 +2,12 @@ import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
 import data from "../db/data.js";
+import stripe from "stripe";
 
 const router = express.Router();
 const shopCollection = await db.collection("shop");
+const url = "https://paixamour.netlify.app/"
+const stripeSecret = await stripe("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");
 
 /*
 router.get("/", async (req, res) => {
@@ -33,6 +36,30 @@ router.get("/Item/:item", async (req, res) => {
   const prodName = req.params.item;
   const results = await shopCollection.find({"item": {$regex: prodName, $options: "i"}}).toArray();
   res.send(results).status(200);
+});
+
+router.post("/Checkout", async (req, res) => {
+  const cartItems = req.body.items.line_items;
+  console.log("Whats the cartItems obj");
+  console.log(cartItems);
+
+  res.send("Checkout endpoint located").status(200);
+
+
+  /*const session = await stripeSecret.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: pr_1234,
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${url}/success`,
+    cancel_url: `${url}/cancel`,
+  });
+
+  res.redirect(303, session.url);*/
 });
 
 export default router;
